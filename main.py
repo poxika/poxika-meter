18de0359e899594869c3c5fb41d0560796050d37
jc martin <jemartin@ebay.com>
2013-10-06 20:58:39 -0700

    Added SMS notification upon detection of frozen stream
    In addition, moved parameters in Config store.

--- main.py
+++ main.py
@@ -14,19 +14,36 @@ TODO : configure COSM API keys through the GAE DB
 from framework import bottle
 from framework.bottle import request, response, Bottle
 from time import mktime, gmtime
-from datetime import datetime
+from datetime import datetime,timedelta
+import logging
 import httplib
 import json
 from google.appengine.ext import db
 from google.appengine.api import taskqueue
 
+from twilio.rest import TwilioRestClient
 
-APIKEY = "AIVLMfLavxYz9-pQ7MNyPlIu8liSAKw0VThoV25wZ0xpaz0g"
 app = Bottle()
+
 
 class Sensor(db.Model):
     timestamp = db.DateTimeProperty()
     lastValues = db.StringProperty()
+
+
+class Config(db.Model):
+    sid = db.StringProperty()
+    token = db.StringProperty()
+    from_number = db.StringProperty()
+    to_number = db.StringProperty()
+    noop = db.BooleanProperty()
+
+
+class Stream(db.Model):
+    frozen = db.BooleanProperty()
+    sent = db.BooleanProperty()
+    errorcount = db.IntegerProperty()
+
 
 class cosm(object):
     """
@@ -67,7 +84,7 @@ class cosm(object):
         """
 
         try:
-            headers = {'X-ApiKey':key,'Content-Type':contentType}
+            headers = {'X-ApiKey': key ,'Content-Type':contentType}
 
             h = httplib.HTTPConnection("api.cosm.com", timeout=timeout)
             h.request('PUT',
@@ -104,12 +121,12 @@ class cosm(object):
 @app.get('/')
 def status():
     """
-    Get GAE GMT time - used like a poor man NTP 
-    return epoch time
+    Application Status
     """
+    sendSMS('test')
     response.content_type = "text/plain"
     d = long(mktime(gmtime()))
-    return "%s"%d
+    return "%s" % d
 
 @app.put('/v2/feeds/<feedid>')
 def feedPUT(feedid):
@@ -123,10 +140,10 @@ def feedPUT(feedid):
         return('No Data')
 
     body = {
-        'feedid':feedid,
-        'body':d,
-        'key':cosm.get_key(request.headers),
-        'contentType':request.headers.get('Content-Type')
+        'feedid': feedid,
+        'body': d,
+        'key': cosm.get_key(request.headers),
+        'contentType': request.headers.get('Content-Type')
     }
 
     taskqueue.add(url='/admin/cosm', method='POST', payload=json.dumps(body))
@@ -167,7 +184,84 @@ def cosmPOST():
     response.status = 200
     return 'OK'
 
+@app.get('/admin/heartbeat')
+def heartbeat():
+
+    sensors = Sensor.all()
+    now = datetime.utcnow()
+    delta = timedelta(minutes=10)
+    frozen = False
+    stream_name = None
+    for s in sensors:
+        key_name = s.key().name()
+        stream_name = key_name.split(':')[0]
+        if s.timestamp + delta < now:
+            logging.debug('Frozen stream: %s - last seen: %s' % (stream_name,
+                                                                 s.timestamp))
+            frozen = True
+            break
+    stream = Stream.get_by_key_name(stream_name)
+    if stream is None:
+        logging.debug('New stream. Creating %s' % stream_name)
+        ns = Stream(key_name=stream_name)
+        ns.errorcount = 0 if not frozen else 1
+        ns.frozen = frozen
+        ns.sent = False
+        ns.put()
+    else:
+        logging.debug('Found stream %s' % stream_name)
+        if frozen:
+            if stream.errorcount > 3:
+                if not stream.sent:
+                    sendSMS('Stream %s Frozen' % stream_name)
+                    stream.sent = True
+            stream.frozen = frozen
+            stream.errorcount = int(stream.errorcount) + 1
+            stream.put()
+        else:
+            if stream.frozen:
+                # was frozen before
+                sendSMS('Stream %s OK : errors %d' % (stream_name,
+                                                      int(stream.errorcount)))
+                stream.sent = False
+                stream.frozen = frozen
+                stream.errorcount = 0
+                stream.put()
+    response.status = 200
+    return 'OK'
+
+@app.get('/admin/init')
+def init():
+
+    response.status = 200
+    return 'OK'
+
+
+def sendSMS(text):
+
+    noop = None
+    try:
+        query = Config.all()
+        query.run(limit=1)
+        c = list(query)[0]
+        account_sid = c.sid
+        auth_token = c.token
+        from_number = c.from_number
+        to_number = c.to_number
+        noop = c.noop
+    except:
+        logging.error('Cannot retrieve config')
+    if not noop:
+        client = TwilioRestClient(account_sid, auth_token)
+        logging.debug('Sending SMS')
+        rv = client.sms.messages.create(to=to_number,
+                                        from_=from_number,
+                                        body=text)
+
+    return 'OK'
+
 def main():
+
     bottle.run(app=app, server='gae')
 
 if __name__ == "__main__":
